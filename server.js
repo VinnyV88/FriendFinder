64c27d6a83324193ad6841a85326b5734a120a6f
vincent_visconti <vinnyv88@gmail.com>
2017-02-27 11:23:31 -0500

    working on modal

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ var path = require("path");
 // Tells node that we are creating an "express" server
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/app/public"));
+
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
