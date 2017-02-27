579249e2b3171bbede7d22967f9e23a80b54a6a7
vincent_visconti <vinnyv88@gmail.com>
2017-02-27 12:55:14 -0500

    cleaned up some code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,3 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 var friendsData = require("../data/friends.js");
 
 // ===============================================================================
@@ -11,23 +5,10 @@ var friendsData = require("../data/friends.js");
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the friendsData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
     // This will be used to handle incoming survey results. 
@@ -55,12 +36,7 @@ module.exports = function(app) {
     friendsData.push(newFriend);
     //display best match
     res.send(friendsData[currentMatch]);
-    console.log("best match is: " + friendsData[currentMatch].name); 
   });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
 
   app.post("/api/clear", function() {
     // Empty out the arrays of data
